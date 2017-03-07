02d5a66f4cd52a84f2709dba95b6e155f3660c59
Enhex <enhex93@gmail.com>
2017-03-07 22:16:40 +0200

    use type alias instead of member type for the underlying type, and use alias template to remove the need to access the type alias.

--- small_int/Source.cpp
+++ small_int/Source.cpp
@@ -6,13 +6,13 @@ using namespace std;
 template<long long max, long long min = 0>
 void print_small_int()
 {
-	cout << "(max=" << max << ", min=" << min << ") -> " << typeid(small_int<max, min>::v).name() << " " << sizeof(small_int<max, min>) << endl;
+	cout << "(max=" << max << ", min=" << min << ") -> " << typeid(small_int<max, min>).name() << " " << sizeof(small_int<max, min>) << endl;
 }
 
 template<unsigned long long max>
 void print_small_uint()
 {
-	cout << "max=" << max << " -> " << typeid(small_int<max>::v).name() << " " << sizeof(small_int<max>) << endl;
+	cout << "max=" << max << " -> " << typeid(small_int<max>).name() << " " << sizeof(small_int<max>) << endl;
 }
 
 #define PRINT_TYPE_UINT(type) cout << "(" << #type << ") "; print_small_uint<numeric_limits<type>::max()>()
@@ -36,6 +36,7 @@ int main()
 	print_small_int<0, -1>();
 	print_small_int<1, -1>();
 	print_small_int<1, -1000>();
+	cout << endl;
 	PRINT_TYPE_INT(char);
 	PRINT_TYPE_INT(short);
 	PRINT_TYPE_INT(int);
@@ -61,8 +62,4 @@ int main()
 	// constexpr
 	constexpr unsigned ce = 10;
 	small_int<ce> a = ce;
-
-	// use underlying type directly
-	SMALL_INT(ce) b = 2;
-	SMALL_INT(ce, -1000) c = -500;
 }
--- small_int/small_int.h
+++ small_int/small_int.h
@@ -9,39 +9,9 @@
 /*
 small_int will automatically choose at compile time the smallest integer type that can contain a given range.
 */
-template<typename T>
-struct small_int_base
-{
-	static_assert(std::is_integral<T>::value,
-		"small_int_base can only be instantiated with an integral type.");
-
-	constexpr small_int_base(T value) : v(std::move(value)) {}
-	
-
-	T& operator=(T other) {
-		v = other;
-		return v;
-	}
-	T& operator=(T&& other) {
-		v = other;
-		return v;
-	}
-
-	T v;
-
-	// Convert to the underlying value
-	operator T& () noexcept { return v; }
-	constexpr operator const T& () const noexcept { return v; }
-
-	// Access members of the underlying type
-	T* operator->() { return &value; }
-	constexpr const T* operator->() const { return &value; }
-};
-
-
 template<long long max_value, long long min_value = 0, typename Enable = void>
-struct small_int : public small_int_base<size_t> {
-	using small_int_base::small_int_base;
+struct small_int_t {
+	using type = size_t;
 	static_assert(max_value > min_value, "small_int can't contain the value range");
 };
 
@@ -50,25 +20,23 @@ struct small_int : public small_int_base<size_t> {
 
 // char
 template<long long max_value, long long min_value>
-struct small_int<max_value, min_value, typename std::enable_if<(
+struct small_int_t<max_value, min_value, typename std::enable_if<(
 	max_value > min_value &&
 	min_value < 0 &&
 	min_value >= std::numeric_limits<char>::min() &&
 	max_value <= std::numeric_limits<char>::max()
-	)>::type> :
-	public small_int_base<char> {
-	using small_int_base::small_int_base;
+	)>::type> {
+	using type = char;
 };
 
 // unsigned char
 template<long long max_value, long long min_value>
-struct small_int<max_value, min_value, typename std::enable_if<(
+struct small_int_t<max_value, min_value, typename std::enable_if<(
 	max_value > min_value &&
 	min_value >= 0 &&
 	max_value <= std::numeric_limits<unsigned char>::max()
-	)>::type> :
-	public small_int_base<unsigned char> {
-	using small_int_base::small_int_base;
+	)>::type> {
+	using type = unsigned char;
 };
 
 
@@ -76,7 +44,7 @@ struct small_int<max_value, min_value, typename std::enable_if<(
 
 // short
 template<long long max_value, long long min_value>
-struct small_int<max_value, min_value, typename std::enable_if<(
+struct small_int_t<max_value, min_value, typename std::enable_if<(
 	max_value > min_value &&
 	min_value < 0 && ((
 		min_value < std::numeric_limits<char>::min() &&
@@ -84,21 +52,19 @@ struct small_int<max_value, min_value, typename std::enable_if<(
 		|| (
 		max_value > std::numeric_limits<char>::max() &&
 		max_value <= std::numeric_limits<short>::max())
-	))>::type> :
-	public small_int_base<short> {
-	using small_int_base::small_int_base;
+	))>::type> {
+	using type = short;
 };
 
 // unsigned short
 template<long long max_value, long long min_value>
-struct small_int<max_value, min_value, typename std::enable_if<(
+struct small_int_t<max_value, min_value, typename std::enable_if<(
 	max_value > min_value &&
 	min_value >= 0 &&
 	max_value > std::numeric_limits<unsigned char>::max() &&
 	max_value <= std::numeric_limits<unsigned short>::max()
-	)>::type> :
-	public small_int_base<unsigned short> {
-	using small_int_base::small_int_base;
+	)>::type> {
+	using type = unsigned short;
 };
 
 
@@ -106,7 +72,7 @@ struct small_int<max_value, min_value, typename std::enable_if<(
 
 // int
 template<long long max_value, long long min_value>
-struct small_int<max_value, min_value, typename std::enable_if<(
+struct small_int_t<max_value, min_value, typename std::enable_if<(
 	max_value > min_value &&
 	min_value < 0 && ((
 		min_value < std::numeric_limits<short>::min() &&
@@ -114,21 +80,19 @@ struct small_int<max_value, min_value, typename std::enable_if<(
 		|| (
 		max_value > std::numeric_limits<short>::max() &&
 		max_value <= std::numeric_limits<int>::max())
-	))>::type> :
-	public small_int_base<int> {
-	using small_int_base::small_int_base;
+	))>::type> {
+	using type = int;
 };
 
 // unsigned int
 template<long long max_value, long long min_value>
-struct small_int<max_value, min_value, typename std::enable_if<(
+struct small_int_t<max_value, min_value, typename std::enable_if<(
 	max_value > min_value &&
 	min_value >= 0 &&
 	max_value > std::numeric_limits<unsigned short>::max() &&
 	max_value <= std::numeric_limits<unsigned int>::max()
-	)>::type> :
-	public small_int_base<unsigned int> {
-	using small_int_base::small_int_base;
+	)>::type> {
+	using type = unsigned int;
 };
 
 
@@ -136,7 +100,7 @@ struct small_int<max_value, min_value, typename std::enable_if<(
 
 // long
 template<long long max_value, long long min_value>
-struct small_int<max_value, min_value, typename std::enable_if<(
+struct small_int_t<max_value, min_value, typename std::enable_if<(
 	max_value > min_value &&
 	min_value < 0 && ((
 		min_value < std::numeric_limits<int>::min() &&
@@ -144,21 +108,19 @@ struct small_int<max_value, min_value, typename std::enable_if<(
 		|| (
 		max_value > std::numeric_limits<int>::max() &&
 		max_value <= std::numeric_limits<long>::max())
-	))>::type> :
-	public small_int_base<long> {
-	using small_int_base::small_int_base;
+	))>::type> {
+	using type = long;
 };
 
 // unsigned long
 template<long long max_value, long long min_value>
-struct small_int<max_value, min_value, typename std::enable_if<(
+struct small_int_t<max_value, min_value, typename std::enable_if<(
 	max_value > min_value &&
 	min_value >= 0 &&
 	max_value > std::numeric_limits<unsigned int>::max() &&
 	max_value <= std::numeric_limits<unsigned long>::max()
-	)>::type> :
-	public small_int_base<unsigned long> {
-	using small_int_base::small_int_base;
+	)>::type> {
+	using type = unsigned long;
 };
 
 
@@ -166,7 +128,7 @@ struct small_int<max_value, min_value, typename std::enable_if<(
 
 // long long
 template<long long max_value, long long min_value>
-struct small_int<max_value, min_value, typename std::enable_if<(
+struct small_int_t<max_value, min_value, typename std::enable_if<(
 	max_value > min_value &&
 	min_value < 0 && ((
 		min_value < std::numeric_limits<long>::min() &&
@@ -174,31 +136,28 @@ struct small_int<max_value, min_value, typename std::enable_if<(
 		|| (
 		max_value > std::numeric_limits<long>::max() &&
 		max_value <= std::numeric_limits<long long>::max())
-	))>::type> :
-	public small_int_base<long long> {
-	using small_int_base::small_int_base;
+	))>::type> {
+	using type = long long;
 };
 
 // unsigned long long
 //NOTE: can't map values greater than long long's max
 template<long long max_value, long long min_value>
-struct small_int<max_value, min_value, typename std::enable_if<(
+struct small_int_t<max_value, min_value, typename std::enable_if<(
 	max_value > min_value &&
 	min_value >= 0 &&
 	max_value > std::numeric_limits<unsigned long>::max() &&
 	max_value <= std::numeric_limits<long long>::max()
-	)>::type> :
-	public small_int_base<unsigned long long> {
-	using small_int_base::small_int_base;
+	)>::type> {
+	using type = unsigned long long;
 };
 
 
-// Marco for directly using the underlying type
-#define EXPAND(x) x
-#define GET_SMALL_INT_MACRO(_1, _2, NAME, ...) NAME
-#define SMALL_INT(...) EXPAND(GET_SMALL_INT_MACRO(__VA_ARGS__, SMALL_INT_2, SMALL_INT_1)(__VA_ARGS__))
 
-#define SMALL_INT_1(max_value) decltype(small_int<max_value>::v)
-#define SMALL_INT_2(max_value, min_value) decltype(small_int<max_value, min_value>::v)
+
+// Alias template to remove the need to access small_int_t::type
+template<long long max_value, long long min_value = 0>
+using small_int = typename small_int_t<max_value, min_value>::type;
+
 
 #endif//small_int_h
